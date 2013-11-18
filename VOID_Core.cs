8cc5054a1ced54d7834100715ac9fb7b4f9a1fe5
Andy Wilkinson <andy@wtribe.com>
2013-11-17 20:46:30 -0800

    Version 0.9.12

--- VOID_Core.cs
+++ VOID_Core.cs
@@ -68,7 +68,7 @@ namespace VOID
 		 * Fields
 		 * */
 		protected string VoidName = "VOID";
-		protected string VoidVersion = "0.9.11";
+		protected string VoidVersion = "0.9.12";
 
 		protected bool _factoryReset = false;
 
