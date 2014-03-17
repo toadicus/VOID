02a4db719dfc0958fcd94b37212a9b11a2e6832f
Andy <andy@wtribe.com>
2014-03-17 15:29:15 -0700

    Version 0.9.21

--- VOID_Core.cs
+++ VOID_Core.cs
@@ -69,7 +69,7 @@ namespace VOID
 		 * Fields
 		 * */
 		protected string VoidName = "VOID";
-		protected string VoidVersion = "0.9.20";
+		protected string VoidVersion = "0.9.21";
 
 		protected bool _factoryReset = false;
 
